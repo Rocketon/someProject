ffb2c588a9b37774ba30577161cf04447ce7311a
Rocketon <goblin9855@gmail.com>
2025-07-30 19:17:19 +0600

    feat(Header): add Filter, styling max-width

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
 import { Logo } from './Logo';
+import { Filter } from '../filter';
 
 export function Header() {
   return (
     <HeaderContainer>
       <Logo />
+      <Filter />
     </HeaderContainer>
   );
 }
@@ -12,6 +14,11 @@ export function Header() {
 const HeaderContainer = styled.div`
   width: 100%;
   display: flex;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
+
+  @media (max-width: 1090px) {
+    flex-direction: column;
+    gap: 30px;
+  }
 `;
